a12d4f55c985d1a2596cf0bd81e44eb925501720
Crow Rising <miscbyrd@gmail.com>
2023-07-28 14:47:28 -0600

    feat: add store finder service

---
+++ app/services/store_service.rb
@@ -0,0 +1,20 @@
+class StoreService
+  
+  def get_location(location, radius)
+    get_url("textsearch/json?query=hardware%20store%20location=#{location}&radius=#{radius}key=#{ENV['GOOGLE_API_KEY']}")
+  end
+
+  private
+  
+  def conn 
+    Faraday.new(url: "https://maps.googleapis.com/maps/api/place/") do |f|
+      f.params['key'] = ENV['GOOGLE_API_KEY']
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
