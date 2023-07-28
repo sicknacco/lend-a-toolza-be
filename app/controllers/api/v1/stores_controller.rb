847132ee44910b464c978bd0f25f2324b589dbd7
Nick Sacco <nicksacco10@gmail.com>
2023-07-28 16:44:22 -0700

    feat:Index action wip

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::StoresController < ApplicationController
   def index
-    
+    store_facade = StoreFacade.new(params[:location], params[:radius])
+    render json: StoreSerializer.new(store_facade.stores)
   end
 end
