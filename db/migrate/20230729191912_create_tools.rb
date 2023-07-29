72834c25b7bb82fe35731e9977dc78164688fb31
Crow Rising <miscbyrd@gmail.com>
2023-07-29 15:38:57 -0600

    style: rubocop sweep

--- db/migrate/20230729191912_create_tools.rb
+++ db/migrate/20230729191912_create_tools.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateTools < ActiveRecord::Migration[7.0]
   def change
     create_table :tools do |t|
--- db/migrate/20230729193845_change_default_borrower.rb
+++ db/migrate/20230729193845_change_default_borrower.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeDefaultBorrower < ActiveRecord::Migration[7.0]
   def change
     change_column_default :tools, :borrower_id, default: nil
