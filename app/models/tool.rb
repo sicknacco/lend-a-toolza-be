7e51f16b1102e800791f95a169dd938ff9e3a77c
Crow Rising <miscbyrd@gmail.com>
2023-07-29 13:42:37 -0600

    wip

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,2 +1,9 @@
 class Tool < ApplicationRecord
+  validates :name, 
+            :description, 
+            :image, 
+            :status, 
+            :address, 
+            :user_id, 
+            :borrower_id, presence: true
 end
---
+++ db/migrate/20230729193845_change_default_borrower.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultBorrower < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :tools, :borrower_id, default: nil
+  end
+end
