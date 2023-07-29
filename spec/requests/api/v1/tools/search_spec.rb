41f5f32ef3ef9c19d3810947b4d4d751541b0ac9
Nick Sacco <nicksacco10@gmail.com>
2023-07-29 14:55:17 -0700

    test: complete tool search request tests

--- spec/requests/api/v1/tools/search_spec.rb
+++ spec/requests/api/v1/tools/search_spec.rb
@@ -6,12 +6,30 @@ RSpec.describe 'Tool Search', type: :request do
   describe 'GET /api/v1/tools/search' do
     let!(:tool_in_colorado) { create(:tool, name: 'Hammer', address: 'Denver, CO') }
     let!(:tool_in_california) { create(:tool, name: 'Screwdriver', address: '90210') }
+    let!(:tool_in_nm) { create(:tool, name: 'Drill Press', address: 'NM, 87501') }
 
-    it 'returns a list of tools that match the search criteria' do
+    it 'returns a list of tools by name and state' do
       get '/api/v1/tools/search?name=Hammer&location=Denver,%20CO'
-
+      
       expect(response).to have_http_status(200)
       expect(response.body).to include(tool_in_colorado.name)
+      expect(response.body).to_not include(tool_in_california.name)
+    end
+    
+    it 'returns a list of tools by name and zip' do
+      get '/api/v1/tools/search?name=Screwdriver&location=90210'
+      
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(tool_in_california.name)
+      expect(response.body).to_not include(tool_in_colorado.name)
+    end
+    
+    it 'returns a list of tools by name, state, and zip' do
+      get '/api/v1/tools/search?name=Drill%20Press&location=NM,%2087501'
+
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(tool_in_nm.name)
+      expect(response.body).to_not include(tool_in_colorado.name)
       expect(response.body).to_not include(tool_in_california.name)
     end
   end
