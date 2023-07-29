b2a672165e1eb13f77deef5f342a51ff29bd7ec0
Crow Rising <miscbyrd@gmail.com>
2023-07-29 13:23:13 -0600

    chore: create database

---
+++ db/migrate/20230729191912_create_tools.rb
@@ -0,0 +1,15 @@
+class CreateTools < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tools do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.string :status
+      t.integer :user_id
+      t.string :address
+      t.integer :borrower_id
+
+      t.timestamps
+    end
+  end
+end
