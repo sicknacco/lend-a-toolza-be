7a662e5230fb6cc5b06e2fa894fb87039cf29055
Crow Rising <miscbyrd@gmail.com>
2023-07-29 13:23:53 -0600

    wip: create spec files

---
+++ spec/factories/tools.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :tool do
+    name { "MyString" }
+    description { "MyString" }
+    image { "MyString" }
+    status { "MyString" }
+    user_id { 1 }
+    address { "MyString" }
+    borrower_id { 1 }
+  end
+end
