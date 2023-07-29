ade723c16f7629f785011a1c2f74639a7a2daac8
Nick Sacco <nicksacco10@gmail.com>
2023-07-29 15:07:44 -0700

    test: add sad path test for tool search

--- spec/requests/api/v1/tools/search_spec.rb
+++ spec/requests/api/v1/tools/search_spec.rb
@@ -26,11 +26,19 @@ RSpec.describe 'Tool Search', type: :request do
     
     it 'returns a list of tools by name, state, and zip' do
       get '/api/v1/tools/search?name=Drill%20Press&location=NM,%2087501'
-
+      
       expect(response).to have_http_status(200)
       expect(response.body).to include(tool_in_nm.name)
       expect(response.body).to_not include(tool_in_colorado.name)
       expect(response.body).to_not include(tool_in_california.name)
+    end
+    
+    it 'displays an error message if no tools are found' do
+      get '/api/v1/tools/search?name=Jackhammer&location=90210'
+      
+      expect(response).to have_http_status(:not_found)
+      expect(response.body).to include("No tools found in your area")
+      expect(response.body).to_not include("Jackhammer")
     end
   end
 end
