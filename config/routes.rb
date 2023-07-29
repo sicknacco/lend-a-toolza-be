9b8a8e032867b29fee19921b060c706ff33c82c4
Crow Rising <miscbyrd@gmail.com>
2023-07-29 15:39:18 -0600

    style: rubocop sweep

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :tools
-  get "/api/v1/stores/:location/:radius", to: "api/v1/stores#index"
-  get "/api/v1/chat_request", to: "api/v1/chats#show"
+  get '/api/v1/stores/:location/:radius', to: 'api/v1/stores#index'
+  get '/api/v1/chat_request', to: 'api/v1/chats#show'
+  get '/api/v1/tools/search', to: 'api/v1/tools#search'
 end
