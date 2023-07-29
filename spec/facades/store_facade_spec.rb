fdfd1763bfbb029b9a13ddd4f7b7852d7ae08873
Crow Rising <miscbyrd@gmail.com>
2023-07-28 18:27:48 -0600

    fix: return data changed

--- spec/facades/store_facade_spec.rb
+++ spec/facades/store_facade_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe StoreFacade, :vcr do
       expect(stores).to be_an Array
       expect(stores.first.name).to eq("Ace Hardware")
       expect(stores.first.formatted_address).to eq("1040 15th St, Denver, CO 80202, United States")
-      expect(stores.last.name).to eq("Edgewater Ace Hardware")
-      expect(stores.last.formatted_address).to eq("1719 Sheridan Boulevard, Edgewater, CO 80214, United States")
+      expect(stores.last.name).to eq("Chamber's Place Ace Hardware")
+      expect(stores.last.formatted_address).to eq("4830 Chambers Rd, Denver, CO 80239, United States")
     end
   end
 end
