f02ba8d3d4ce2073e08bf82cbb732d02f9bc3975
Crow Rising <miscbyrd@gmail.com>
2023-07-29 13:41:50 -0600

    test: add validation test

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tool, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :borrower_id }
+  end
 end
