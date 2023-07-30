eaab164363de24fb6dc4e3f0358008a75941abd2
Nick Sacco <nicksacco10@gmail.com>
2023-07-30 14:53:36 -0700

    style/refactor: rubocop sweep

--- spec/requests/api/v1/tools/search_spec.rb
+++ spec/requests/api/v1/tools/search_spec.rb
@@ -10,40 +10,40 @@ RSpec.describe 'Tool Search', type: :request do
 
     it 'returns a list of tools by name and state' do
       get '/api/v1/tools/search?name=Hammer&location=Denver,%20CO'
-      
+
       expect(response).to have_http_status(200)
       expect(response.body).to include(tool_in_colorado.name)
       expect(response.body).to_not include(tool_in_california.name)
     end
-    
+
     it 'returns a list of tools by name and zip' do
       get '/api/v1/tools/search?name=Screwdriver&location=90210'
-      
+
       expect(response).to have_http_status(200)
       expect(response.body).to include(tool_in_california.name)
       expect(response.body).to_not include(tool_in_colorado.name)
     end
-    
+
     it 'returns a list of tools by name, state, and zip' do
       get '/api/v1/tools/search?name=Drill%20Press&location=NM,%2087501'
-      
+
       expect(response).to have_http_status(200)
       expect(response.body).to include(tool_in_nm.name)
       expect(response.body).to_not include(tool_in_colorado.name)
       expect(response.body).to_not include(tool_in_california.name)
     end
-    
+
     it 'displays an error message if no tools are found' do
       get '/api/v1/tools/search?name=Jackhammer&location=90210'
-      
+
       expect(response).to have_http_status(:not_found)
-      expect(response.body).to include("No tools found in your area")
-      expect(response.body).to_not include("Jackhammer")
+      expect(response.body).to include('No tools found in your area')
+      expect(response.body).to_not include('Jackhammer')
     end
   end
 
   describe 'Single Tool Search' do
-    it "returns a single tool and its attributes" do
+    it 'returns a single tool and its attributes' do
       tool = create(:tool, user_id: 1)
       get "/api/v1/tools/search?name=#{tool.name}&location=#{tool.address}"
 
