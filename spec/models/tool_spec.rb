8c9989313d63c74dc32cf0b2291e6d4e0054afa6
Nick Sacco <nicksacco10@gmail.com>
2023-07-30 15:06:52 -0700

    test: model tests for tool search functionality

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -12,4 +12,40 @@ RSpec.describe Tool, type: :model do
     it { should validate_presence_of :user_id }
     it { should validate_presence_of :borrower_id }
   end
+
+  describe 'class methods' do
+    let!(:tool_in_colorado) { create(:tool, name: 'Hammer', address: 'Denver, CO') }
+    let!(:tool_in_california) { create(:tool, name: 'Screwdriver', address: '90210') }
+    let!(:tool_in_nm) { create(:tool, name: 'Drill Press', address: 'NM, 87501') }
+
+    it 'returns a list of tools by name and state' do
+      result = Tool.search_by_name_and_state_or_zip('Hammer', 'Denver, CO')
+      
+      expect(result).to eq([tool_in_colorado])
+      expect(result).to_not eq([tool_in_california])
+      expect(result).to_not eq([tool_in_nm])
+    end
+    
+    it 'returns a list of tools by name and zip' do
+      result = Tool.search_by_name_and_state_or_zip('Screwdriver', '90210')
+      
+      expect(result).to eq([tool_in_california])
+      expect(result).to_not eq([tool_in_colorado])
+      expect(result).to_not eq([tool_in_nm])
+    end
+
+    it 'returns a list of tools by name, state, and zip' do
+      result = Tool.search_by_name_and_state_or_zip('Drill Press', 'NM, 87501')
+
+      expect(result).to eq([tool_in_nm])
+      expect(result).to_not eq([tool_in_colorado])
+      expect(result).to_not eq([tool_in_california])
+    end
+
+    it 'returns an error message if no tools are found' do
+      result = Tool.search_by_name_and_state_or_zip('Jackhammer', '90210')
+
+      expect(result).to be_empty
+    end
+  end
 end
