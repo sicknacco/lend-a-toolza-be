e8a5e3f7011b42525b71eca9f826f0724a0f1d74
Nick Sacco <nicksacco10@gmail.com>
2023-08-03 08:03:32 -0700

    refactor to get working tests after pulling

--- spec/requests/api/v1/chat_request_spec.rb
+++ spec/requests/api/v1/chat_request_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe 'ChatService API', type: :request do
       get "/api/v1/chat_request?project=#{project}"
 
       expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body, symbolize_names: true)).to eq({ tools: ["-Hammer", "-Drill", "-Measuring tape", "-Circular saw", "-Safety glasses", "-Work gloves", "-Level", "-Post hole digger", "-Shovel", "-Screws", "-Nails", "-Deck screws", "-Deck boards", "-Deck railings", "-Deck posts", "-Deck joists", "-Deck footings", "-Deck sealant"
-        ]})
+      expect(JSON.parse(response.body, symbolize_names: true)).to eq({ tools: ["1. Hammer", "2. Nails", "3. Drill", "4. Screws", "5. Circular Saw", "6. Tape Measure", "7. Level", "8. Safety Glasses", "9. Work Gloves", "10. Post Hole Digger", "11. Shovel", "12. Chalk Line", "13. Paintbrush", "14. Paint Roller", "15. Deck Boards", "16. Joist Hangers", "17. Deck Railings", "18. Deck Spindles", "19. Deck Posts", "20. Deck Stair Stringers"
+      ]})
     end
   end
 end
