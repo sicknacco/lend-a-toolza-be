21bf8e435c93c88c3eddb1e1d3be57ede9e76a62
Nick Sacco <nicksacco10@gmail.com>
2023-08-03 13:37:10 -0700

    feat: create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Tool.destroy_all
+
+tool1 = Tool.create!(name: "Hammer", description: "Smashem", image: "image.jpg", status: "available", user_id: 2, address: "123 Sunnyside Dr, Lebanon, IN, 46052", borrower_id: nil)
+tool2 = Tool.create!(name: "Saw", description: "Cutem", image: "image.jpg", status: "available", user_id: 2, address: "123 Sunnyside Dr, Lebanon, IN, 46052", borrower_id: nil)
+tool3 = Tool.create!(name: "Drill", description: "Drillem", image: "image.jpg", status: "available", user_id: 4, address: "1234 Sunnyside Dr, Lebanon, IN, 46052", borrower_id: 2)
+tool4 = Tool.create!(name: "Screwdriver", description: "Screwem", image: "image.jpg", status: "available", user_id: 7, address: "1237 Sunnyside Dr, Lebanon, IN, 46052", borrower_id: 2)
